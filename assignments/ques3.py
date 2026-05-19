247e40c44f545c10bdcc9b72aaf266e3f5e7f2ab
Nandini Gora <goranandini13@gamail.com>
2026-05-20 01:22:14 +0530

    first commit

---
+++ assignments/ques3.py
@@ -0,0 +1,16 @@
+#assignment operators
+a=10
+a+=5
+print(a)
+
+a-=2
+print(a)
+
+a*=3
+print(a)
+
+a/=2
+print(a)
+
+a%=4
+print(a)
---
+++ assignments/ques4.py
@@ -0,0 +1,13 @@
+#conditinal statements
+per=float(input("enter the percentage:"))
+if per >=60:
+    print("grade A")
+elif per >= 50:
+    print("grade B")
+elif per >=40:
+    print("grade C")
+elif per >=33:
+    print("grade D")
+else:
+    print("fail")               
+
